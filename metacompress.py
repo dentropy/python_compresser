3b46fdd8ddc75307b305449084bf9e7344437dff
Paul Mullins <paulmullins@protonmail.com>
2020-04-13 10:29:04 -0400

    Fixed bug where entire directory structure was coppied rather than current directory

--- metacompress.py
+++ metacompress.py
@@ -21,7 +21,7 @@ def unzip_file(file_path, output_path):
 
 end_text = '''
 write_bytes("mah_bytes.zip", mah_bytes)
-unzip_file("mah_bytes.zip", "./what")
+unzip_file("mah_bytes.zip", "./unzipped_files")
 '''
 
 
@@ -50,7 +50,8 @@ def unzip_file(file_path, output_path):
 
 if __name__ == '__main__':
     # https://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory#5137509
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+    #dir_path = os.path.dirname(os.path.realpath(__file__))
+    dir_path = "."
     print(dir_path)
     # The next four lines are from
     # https://stackoverflow.com/questions/1855095/how-to-create-a-zip-archive-of-a-directory-in-python
@@ -59,14 +60,10 @@ if __name__ == '__main__':
     zipf.close()
     print('All files zipped successfully!')  
     some_bytes = get_bytes_from_file("Python.zip") 
-    '''      
-    print(some_bytes)
-    write_bytes('Python2.zip', some_bytes)
-    unzip_file("Python.zip", "./what")
-    '''
     with open("supermeta.py", 'a') as out:
         out.write(beginning_text + '\n')
         out.write("mah_bytes = " + str(some_bytes) + "\n")
         out.write(end_text + '\n')
+    os.remove("Python.zip")
 
 ## Peach Pie is from https://ccsearch.creativecommons.org/photos/fe32017c-df1b-455c-9784-0f9b3a141717
